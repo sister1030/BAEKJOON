de5d0c4f7504d2d785ad6f775724576faa416db8
HIYENA <103008887+sister1030@users.noreply.github.com>
2022-12-07 14:37:46 +0000

    2588

---
+++ Backjoon2588.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Backjoon2588 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+   System.out.println("첫번째줄을 입력하세요");
+    int n1 = sc.nextInt();
+   System.out.println("두번째줄을 입력하세요");
+    int n2 = sc.nextInt();
+    System.out.println(n1*(n2%10));
+    System.out.println(n1*(n2%100/10));     System.out.println(n1*(n2/100));
+  System.out.println(n1*n2);
+  }
+}
